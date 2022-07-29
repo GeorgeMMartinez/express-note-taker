c344a9930287174ff5ac074b43dbce2c37d914c6
Matt Martinez <matt@Matts-MacBook-Pro.local>
2022-07-29 03:21:11 -0700

    fixed error

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+app.get('/', (req, res) => res.sendFile(join(__dirname, 'public', 'index.html')));
 app.get('/notes', (req, res) => res.sendFile(join(__dirname, 'public', 'notes.html')));
 app.get('/api/notes', (req, res) => res.json(newData));
 app.post('/api/notes', function (req, res) {
