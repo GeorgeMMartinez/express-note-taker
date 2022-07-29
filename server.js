0b8e0d8087e4a4b0d3f7cab049d870801de776ca
Matt Martinez <matt@Matts-MacBook-Pro.local>
2022-07-29 01:06:46 -0700

    code refactor

--- server.js
+++ server.js
@@ -1,37 +1,30 @@
-const express = require('express')
-const fs = require('fs')
-const { join } = require('path')
-const { uid } = require('uid')
-// connections
-const app = express()
-let noteData = require('./db/db.json')
-// middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static(join(__dirname, 'public')))
-// paths
-app.get('/', (req, res) => res.sendFile(join(__dirname, 'public', 'index.html')))
-app.get('/notes', (req, res) => res.sendFile(join(__dirname, 'public', 'notes.html')))
-app.get('/api/notes', (req, res) => res.json(noteData))
+// dependencies/node packages
+const express = require('express');
+const fs = require('fs');
+const { join } = require('path');
+const { uid } = require('uid');
+
+let newData = require('./db/db.json');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.sendFile(join(__dirname, 'public', 'index.html')));
+app.get('/notes', (req, res) => res.sendFile(join(__dirname, 'public', 'notes.html')));
+app.get('/api/notes', (req, res) => res.json(newData));
 app.post('/api/notes', (req, res) => {
   let newNote = {
     title: req.body.title,
     text: req.body.text,
     id: uid(),
   }
-  noteData.push(newNote)
-  fs.writeFile(join(__dirname, 'db', 'db.json'), JSON.stringify(noteData), err => {
+  newData.push(newNote)
+  fs.writeFile(join(__dirname, 'db', 'db.json'), JSON.stringify(newData), err => {
     if (err) { console.log(err) }
     res.json(newNote)
   })
 })
 
-// app.delete('/api/notes/:id', (req, res) => {
-//   noteData = noteData.filter(note => note.id != req.params.id)
-//   fs.writeFile(join(__dirname, 'db', 'db.json'), JSON.stringify(noteData), err => {
-//     if (err) { console.log(err) }
-//     res.sendStatus(200)
-//   })
-// })
-// port
-app.listen(process.env.PORT || 3001)
+app.listen(process.env.PORT || 3001);
