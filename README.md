475f41e35a2338574b7351796cbc6c2a427f45cf
Matt Martinez <matt@Matts-MacBook-Pro.local>
2022-07-29 03:16:26 -0700

    added README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Note Taker Starter Code
+Express Note Taker 
+
+This application allows users to create notes on a live site.
+
+The application utilizes an Express.js back end and saves/retrieves note data from a JSON file.
+
+This application is deployed live via Heroku at the following link:
+
+https://dashboard.heroku.com/apps/gmnotetaker
+
+The code can be found at the following GitHub repository:
+
+https://github.com/GeorgeMMartinez/express-note-taker
--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 // dependencies/node packages
 const express = require('express');
 const fs = require('fs');
-const { join } = require('path');
+const path = require('path');
 const { uid } = require('uid');
 
-let newData = require('./db/db.json');
+const newData = require('./db/db.json');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.sendFile(join(__dirname, 'public', 'index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 app.get('/notes', (req, res) => res.sendFile(join(__dirname, 'public', 'notes.html')));
 app.get('/api/notes', (req, res) => res.json(newData));
-app.post('/api/notes', (req, res) => {
-  let newNote = {
+app.post('/api/notes', function (req, res) {
+  let addNewNote = {
     title: req.body.title,
     text: req.body.text,
     id: uid(),
   }
-  newData.push(newNote)
+  newData.push(addNewNote)
   fs.writeFile(join(__dirname, 'db', 'db.json'), JSON.stringify(newData), err => {
     if (err) { console.log(err) }
-    res.json(newNote)
+    res.json(addNewNote)
   })
 })
 
